6ecc5f75d89c83f3fcfef6eb7c31c67b90184d3c
HP-PC\Workstation z620 <nguyentrungquang102@gmail.com>
2018-03-01 10:46:09 +0700

    quangnt pushed the initial of powersuply project, PWM, UART, ADC

--- adc-cfg/Debug/Drivers/STM32F0xx_HAL_Driver/Src/subdir.mk
+++ adc-cfg/Debug/Drivers/STM32F0xx_HAL_Driver/Src/subdir.mk
@@ -63,7 +63,7 @@ Drivers/STM32F0xx_HAL_Driver/Src/%.o: ../Drivers/STM32F0xx_HAL_Driver/Src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F051x8 -I"D:/STM32/workspace/adc-cfg/Inc" -I"D:/STM32/workspace/adc-cfg/Drivers/STM32F0xx_HAL_Driver/Inc" -I"D:/STM32/workspace/adc-cfg/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy" -I"D:/STM32/workspace/adc-cfg/Drivers/CMSIS/Device/ST/STM32F0xx/Include" -I"D:/STM32/workspace/adc-cfg/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F051x8 -I"E:/gitworks/adc-cfg/Inc" -I"E:/gitworks/adc-cfg/Drivers/STM32F0xx_HAL_Driver/Inc" -I"E:/gitworks/adc-cfg/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy" -I"E:/gitworks/adc-cfg/Drivers/CMSIS/Device/ST/STM32F0xx/Include" -I"E:/gitworks/adc-cfg/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
