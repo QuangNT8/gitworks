2a6983a1a419dca34cd618fcb1893dffcc8df167
DELL990\dell <nguyentrungquang102@gmail.com>
2018-03-01 23:46:51 +0700

    removed the testing code

--- adc-cfg/Src/main.c
+++ adc-cfg/Src/main.c
@@ -123,7 +123,7 @@ int main(void)
   {
 
   /* USER CODE END WHILE */
-	  HAL_Delay(200); //quangnt test git
+	  HAL_Delay(200);
   /* USER CODE BEGIN 3 */
 	  HAL_ADC_Start_IT(&hadc);
 	  counter = __HAL_TIM_GET_COUNTER(&htim1);
