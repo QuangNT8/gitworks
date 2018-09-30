e46c844239348d85815f13ecedb11a057024e352
DELL990\dell <nguyentrungquang102@gmail.com>
2018-09-30 09:26:58 +0700

    updated for ADC module

--- nrf52-QT/mylib/adc/nrf_drv_saadc.c
+++ nrf52-QT/mylib/adc/nrf_drv_saadc.c
@@ -1,7 +1,74 @@
 /*QuangNT*/
 #include "nrf_saadc.h"
 #include "nrf_drv_saadc.h"
-void mysaadc_init(void)
+
+typedef enum
+{
+    NRF_SAADC_STATE_IDLE        = 0,
+    NRF_SAADC_STATE_BUSY        = 1,
+    NRF_SAADC_STATE_CALIBRATION = 2
+} nrf_saadc_state_t;
+typedef struct
+{
+    nrf_saadc_input_t pselp;
+    nrf_saadc_input_t pseln;
+} nrf_saadc_psel_buffer;
+/** @brief SAADC control block.*/
+
+typedef struct
+{
+    nrf_drv_saadc_event_handler_t event_handler;                 ///< Event handler function pointer.
+    volatile nrf_saadc_value_t  * p_buffer;                      ///< Sample buffer.
+    volatile uint16_t             buffer_size;                   ///< Size of the sample buffer.
+    volatile nrf_saadc_value_t  * p_secondary_buffer;            ///< Secondary sample buffer.
+    volatile nrf_saadc_state_t    adc_state;                     ///< State of the SAADC.
+    uint32_t                      limits_enabled_flags;          ///< Enabled limits flags.
+    uint16_t                      secondary_buffer_size;         ///< Size of the secondary buffer.
+    uint16_t                      buffer_size_left;              ///< When low power mode is active indicates how many samples left to convert on current buffer.
+    nrf_saadc_psel_buffer         psel[NRF_SAADC_CHANNEL_COUNT]; ///< Pin configurations of SAADC channels.
+    nrf_drv_state_t               state;                         ///< Driver initialization state.
+    uint8_t                       active_channels;               ///< Number of enabled SAADC channels.
+    bool                          low_power_mode;                ///< Indicates if low power mode is active.
+    bool                          conversions_end;               ///< When low power mode is active indicates end of conversions on current buffer.
+} nrf_drv_saadc_cb_t;
+
+static nrf_drv_saadc_cb_t m_cb;
+
+void SAADC_IRQHandler(void)
+{
+    if (nrf_saadc_event_check(NRF_SAADC_EVENT_END))
+    {
+        nrf_saadc_event_clear(NRF_SAADC_EVENT_END);
+        if (m_cb.conversions_end)
+        {           
+            nrf_drv_saadc_evt_t evt;
+            evt.type               = NRF_DRV_SAADC_EVT_DONE;
+            evt.data.done.p_buffer = (nrf_saadc_value_t *)m_cb.p_buffer;
+            evt.data.done.size     = m_cb.buffer_size;
+            if (m_cb.p_secondary_buffer == NULL)
+            {
+                m_cb.adc_state = NRF_SAADC_STATE_IDLE;
+            }
+            else
+            {
+                m_cb.buffer_size_left   = m_cb.secondary_buffer_size;
+                m_cb.p_buffer           = m_cb.p_secondary_buffer;
+                m_cb.buffer_size        = m_cb.secondary_buffer_size;
+                m_cb.p_secondary_buffer = NULL;
+                if (!m_cb.low_power_mode)
+                {
+                    nrf_saadc_task_trigger(NRF_SAADC_TASK_START);
+                }
+            }
+            m_cb.event_handler(&evt);
+            m_cb.conversions_end = false;
+        }
+    }
+}
+
+
+void mysaadc_init(nrf_drv_saadc_config_t const * p_config,
+                  nrf_drv_saadc_event_handler_t  event_handler)
 {
     nrf_saadc_resolution_t resolution;
     nrf_saadc_oversample_t oversample;
@@ -32,5 +99,20 @@ void mysaadc_init(void)
     nrf_saadc_oversample_set(oversample);
 
     nrf_saadc_channel_init(0, &channel_0_config);
+    nrf_saadc_channel_init(1, &channel_1_config);
+    nrf_saadc_channel_init(2, &channel_2_config);
+    nrf_saadc_channel_init(3, &channel_3_config);
+
+    m_cb.event_handler = event_handler;
+    nrf_saadc_resolution_set(p_config->resolution);
+    nrf_saadc_oversample_set(p_config->oversample);
+    m_cb.low_power_mode       = p_config->low_power_mode;
+    m_cb.state                = NRF_DRV_STATE_INITIALIZED;
+    m_cb.adc_state            = NRF_SAADC_STATE_IDLE;
+    m_cb.active_channels      = 0;
+    m_cb.limits_enabled_flags = 0;
+    m_cb.conversions_end      = false;
+
     //nrf_saadc_channel_input_set(0, channel_0_config->pin_p, channel_0_config->pin_n);
 }
+
