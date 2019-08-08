92cd954e2c830ef5c724c1d2b3f6862fe05d6a94
quang102 <nguyentrungquang102@gmail.com>
2019-08-08 17:28:27 +0700

    added example for CAN

---
+++ CAN/STM32F042_testing/Debug/Src/stm32f0xx_hal_msp.d
@@ -0,0 +1,82 @@
+Src/stm32f0xx_hal_msp.o: ../Src/stm32f0xx_hal_msp.c \
+ D:/testst32cube/STM32F042_testing/Inc/main.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ D:/testst32cube/STM32F042_testing/Inc/stm32f0xx_hal_conf.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f042x6.h \
+ D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Include/core_cm0.h \
+ D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_can.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim.h \
+ D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h
+
+D:/testst32cube/STM32F042_testing/Inc/main.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+D:/testst32cube/STM32F042_testing/Inc/stm32f0xx_hal_conf.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f042x6.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Include/core_cm0.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_can.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim.h:
+
+D:/testst32cube/STM32F042_testing/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h:
